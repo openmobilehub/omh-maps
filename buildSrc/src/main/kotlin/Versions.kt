c5ce2de7e6e74f4f9662d61ed991116972a9f111
Hans Hamel <hans.hamel@encora.com>
2023-03-06 16:12:15 -0400

    Maps SDK - Setup dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,20 @@
 object Versions {
+    // Build Plugins
     const val androidGradlePlugin = "7.4.1"
     const val kotlin = "1.7.20"
     const val detekt = "1.22.0"
     const val jacoco = "0.8.6"
+
+    //KTX
+    const val coreKtx = "1.9.0"
+    const val lifecycleKtx = "2.5.1"
+
+    // Android
+    const val androidAppCompat = "1.6.1"
+    const val material = "1.8.0"
+
+    // Testing
+    const val junit = "4.13.2"
+    const val androidJunit = "1.1.5"
+    const val espresso = "3.5.1"
 }
