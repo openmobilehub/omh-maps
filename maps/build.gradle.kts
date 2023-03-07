25b4c35965b8ce476116e8d50701515cae4fd326
hans-hamel <124717244+hans-hamel@users.noreply.github.com>
2023-03-07 11:49:33 -0400

    Setup dependencies (#1)
    * Maps SDK - Add missing git ignore for mac os
    
    * Maps SDK - Setup dependencies
    
    * Maps SDK - Remove a dependency that is not needed

--- maps/build.gradle.kts
+++ maps/build.gradle.kts
@@ -7,10 +7,14 @@ android {
 }
 
 dependencies {
-    // Remove this dummy dependencies and add your own.
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // KTX
+    implementation(Libs.coreKtx)
+    implementation(Libs.lifecycleKtx)
+
+    implementation(Libs.androidAppCompat)
+    implementation(Libs.material)
+
+    // Test dependencies
+    testImplementation(Libs.junit)
+    androidTestImplementation(Libs.androidJunit)
 }
