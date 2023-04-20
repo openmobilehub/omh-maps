5bf642e8b385403ac394092e190c866c30c598a6
hans-hamel <124717244+hans-hamel@users.noreply.github.com>
2023-04-20 08:40:31 -0400

    Center my location
    * Maps sdk - Center my location

---
+++ maps-api-openstreetmap/src/main/java/com/omh/android/maps/api/openstreetmap/presentation/maps/MyLocationIconOverlay.kt
@@ -0,0 +1,57 @@
+package com.omh.android.maps.api.openstreetmap.presentation.maps
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.view.MotionEvent
+import androidx.core.content.ContextCompat
+import com.omh.android.maps.api.openstreetmap.utils.Constants.PADDING_MY_LOCATION_ICON
+import org.osmdroid.library.R.drawable.ic_menu_mylocation
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Overlay
+
+internal class MyLocationIconOverlay(context: Context) : Overlay() {
+    private val icon: Drawable? = ContextCompat.getDrawable(context, ic_menu_mylocation)
+    private val clickListeners: MutableList<() -> Unit> = mutableListOf()
+
+    override fun draw(canvas: Canvas?, mapView: MapView?, shadow: Boolean) {
+        super.draw(canvas, mapView, shadow)
+
+        if (!shadow && icon != null && mapView != null) {
+            val coordinateX = mapView.width - icon.intrinsicWidth - PADDING_MY_LOCATION_ICON
+            val coordinateY = PADDING_MY_LOCATION_ICON
+
+            icon.setBounds(
+                coordinateX,
+                coordinateY,
+                coordinateX + icon.intrinsicWidth,
+                coordinateY + icon.intrinsicHeight
+            )
+            canvas?.let { icon.draw(it) }
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?, mapView: MapView?): Boolean {
+        if (event?.action == MotionEvent.ACTION_UP && mapView != null && icon != null) {
+            val coordinateX = mapView.width - icon.intrinsicWidth - PADDING_MY_LOCATION_ICON
+            val coordinateY = PADDING_MY_LOCATION_ICON
+            val iconBounds = Rect(
+                coordinateX,
+                coordinateY,
+                coordinateX + icon.intrinsicWidth,
+                coordinateY + icon.intrinsicHeight
+            )
+
+            if (iconBounds.contains(event.x.toInt(), event.y.toInt())) {
+                clickListeners.forEach { onClick -> onClick.invoke() }
+                return true
+            }
+        }
+        return super.onTouchEvent(event, mapView)
+    }
+
+    fun addOnClickListener(onClick: () -> Unit) {
+        clickListeners.add(onClick)
+    }
+}
--- maps-api-openstreetmap/src/main/java/com/omh/android/maps/api/openstreetmap/utils/Constants.kt
+++ maps-api-openstreetmap/src/main/java/com/omh/android/maps/api/openstreetmap/utils/Constants.kt
@@ -7,4 +7,6 @@ internal object Constants {
     const val MIN_DISTANCE_M = 0f
     const val MIN_TIMES_EXECUTION = 1
     const val TIME_OUT_MS = 2000L
+    const val PADDING_MY_LOCATION_ICON = 16
+    const val DEFAULT_ZOOM_LEVEL = 15.0
 }
