80e2e7be23d8038cd5299708810c1542fa8db8ed
Hans Hamel <hans.hamel@encora.com>
2023-05-09 17:26:25 -0400

    Error handling including improvements in the sample app.

--- maps-api-openstreetmap/src/test/java/com/omh/android/maps/api/openstreetmap/LocationManagerTest.kt
+++ maps-api-openstreetmap/src/test/java/com/omh/android/maps/api/openstreetmap/LocationManagerTest.kt
@@ -81,8 +81,8 @@ internal class LocationManagerTest {
 
         try {
             locationManager.getLastKnownLocation()
-        } catch (exception: OmhMapException.ApiException) {
-            assertTrue(exception is OmhMapException.ApiException)
+        } catch (exception: OmhMapException.InvalidArgument) {
+            assertTrue(exception is OmhMapException.InvalidArgument)
         }
     }
 
