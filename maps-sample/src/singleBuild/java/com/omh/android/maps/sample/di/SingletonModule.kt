a76a9d4415c151a65a2675ea106e72ed21645545
Anton Soares <asoares@butterflynetinc.com>
2023-05-11 10:55:05 -0500

    refactored a bit the provider.

--- maps-sample/src/singleBuild/java/com/omh/android/maps/sample/di/SingletonModule.kt
+++ maps-sample/src/singleBuild/java/com/omh/android/maps/sample/di/SingletonModule.kt
@@ -1,11 +1,9 @@
 package com.omh.android.maps.sample.di
 
-import android.content.Context
 import com.omh.android.maps.api.factories.OmhMapProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
@@ -13,10 +11,10 @@ import dagger.hilt.components.SingletonComponent
 class SingletonModule {
 
     @Provides
-    fun omhMapProvider(@ApplicationContext context: Context): OmhMapProvider {
+    fun omhMapProvider(): OmhMapProvider {
         return OmhMapProvider.Builder()
             .addGmsPath()
             .addNonGmsPath()
-            .build(context)
+            .build()
     }
 }
