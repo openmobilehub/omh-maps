561d3f1fa2b853ca140ca124d2c5a93ce58a3e59
Anton Soares <asoares@butterflynetinc.com>
2023-05-11 12:17:13 -0500

    Revert "refactored a bit the provider."
    This reverts commit a76a9d4415c151a65a2675ea106e72ed21645545.

--- maps-sample/src/main/java/com/omh/android/maps/sample/di/OmhMapsModule.kt
+++ maps-sample/src/main/java/com/omh/android/maps/sample/di/OmhMapsModule.kt
@@ -1,23 +1,18 @@
 package com.omh.android.maps.sample.di
 
-import android.content.Context
 import com.omh.android.maps.api.factories.OmhMapProvider
 import com.omh.android.maps.api.presentation.interfaces.location.OmhLocation
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
 
 @Module
 @InstallIn(ActivityComponent::class)
 object OmhMapsModule {
 
     @Provides
-    fun providesOmhLocation(
-        provider: OmhMapProvider,
-        @ApplicationContext context: Context
-    ): OmhLocation {
-        return provider.provideOmhLocation(context)
+    fun providesOmhLocation(provider: OmhMapProvider): OmhLocation {
+        return provider.provideOmhLocation()
     }
 }
