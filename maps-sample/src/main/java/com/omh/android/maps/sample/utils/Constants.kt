8bc8a29adcb7de9726712f99dd3eb280c16692ee
hans-hamel <124717244+hans-hamel@users.noreply.github.com>
2023-05-31 15:33:23 -0400

    Add loading components (#75)
    Maps sdk - Add Fused provider to my location layer in OSM. Add loading icon and toast when is getting current location

--- maps-sample/src/main/java/com/omh/android/maps/sample/utils/Constants.kt
+++ maps-sample/src/main/java/com/omh/android/maps/sample/utils/Constants.kt
@@ -31,4 +31,7 @@ object Constants {
     // Bundle keys
     const val LOCATION_KEY = "location"
     const val ONLY_DISPLAY_KEY = "only display"
+
+    // Loading
+    const val SHOW_MESSAGE_TIME = 5000L
 }
